dc37d01f810e81422f2a423ff856ce2c98b07f5c
Viktor Glazkov <viktorg.dev@gmail.com>
2017-02-28 18:21:14 +0200

    Configure Project entity

--- src/main/java/core/project/Project.java
+++ src/main/java/core/project/Project.java
@@ -4,10 +4,26 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "project")
-public class Project {
+public class  Project {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column
     private Long id;
+
+    private String name;
+
+    public Project(){}
+
+    public Project(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
