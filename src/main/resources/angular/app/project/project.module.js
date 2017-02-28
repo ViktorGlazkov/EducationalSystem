17818522ea5e1203a43b0cb4b5223876c71b603e
Viktor Glazkov <viktorg.dev@gmail.com>
2017-02-28 18:28:33 +0200

    Add project module

---
+++ src/main/resources/angular/app/project/project.module.js
@@ -0,0 +1,15 @@
+angular.module('project', ['ngRoute'])
+    .config(['$routeProvider',
+        function ($routeProvider) {
+            $routeProvider
+                .when('/projects', {
+                    templateUrl: 'app/project/project_list.html',
+                    controller: 'projectController',
+                    controllerAs: 'vm'
+                })
+                .when('/project/:id', {
+                    templateUrl: 'app/project/project_details.html',
+                    controller: 'projectDetailsController',
+                    controllerAs: 'vm'
+                })
+        }]);
