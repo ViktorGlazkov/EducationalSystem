ac74fc2c3c5d840ea4652ae7cad053fa1b939932
Viktor Glazkov <viktorg.dev@gmail.com>
2017-02-28 18:22:22 +0200

    Configure Technology entity

--- src/main/java/core/technology/Technology.java
+++ src/main/java/core/technology/Technology.java
@@ -1,5 +1,8 @@
 package core.technology;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import core.language.Language;
+
 import javax.persistence.*;
 
 @Entity
@@ -10,4 +13,26 @@ public class Technology {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column
     private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JsonBackReference
+    private Language language;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
 }
