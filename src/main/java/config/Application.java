a869dcc7775fdd26ed3505f299b5af12b3c0a12c
Viktor Glazkov <viktorg.dev@gmail.com>
2017-03-04 17:45:35 +0200

    Add Spring Boot app

---
+++ src/main/java/config/Application.java
@@ -0,0 +1,18 @@
+package config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories("/core")
+@EntityScan("/core")
+@ComponentScan(value = "/core")
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
