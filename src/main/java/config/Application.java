4c1f4ca75c9d91e7a0c662842d4baee65cbfc0f4
Viktor Glazkov <viktorg.dev@gmail.com>
2017-03-05 17:42:21 +0200

    Add bean for WebSecurityConfigurerAdapter

--- src/main/java/config/Application.java
+++ src/main/java/config/Application.java
@@ -1,17 +1,29 @@
 package config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@SpringBootApplication
+@Configuration
+@SpringBootApplication()
 @EnableJpaRepositories("/core")
 @EntityScan("/core")
 @ComponentScan(value = "/core")
+@EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
+
+    @Bean
+    public WebSecurityConfigurerAdapter webSecurityConfigurerAdapter() {
+        return new SecurityConfig();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
