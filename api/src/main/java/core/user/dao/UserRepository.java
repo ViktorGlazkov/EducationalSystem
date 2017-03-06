1d2e601e3363559026ca8258fd267663093f03d4
Viktor Glazkov <viktorg.dev@gmail.com>
2017-03-06 11:06:45 +0200

    Remove extra code

--- api/src/main/java/core/user/dao/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package core.user.dao;
-
-import core.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- api/src/main/java/core/user/dao/UserRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package core.user.dao;
-
-import core.user.User;
-
-public interface UserRepositoryCustom {
-    User loadUserByUsername(String username);
-    User findByName(String name);
-}
