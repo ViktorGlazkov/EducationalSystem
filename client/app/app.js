79d23d7a55e1595bab80a2c76b55c717ca84868f
Viktor Glazkov <viktorg.dev@gmail.com>
2017-03-07 01:23:39 +0200

    Add module security

--- client/app/app.js
+++ client/app/app.js
@@ -2,17 +2,12 @@ angular.module('app', [
     'ngRoute',
     'ngResource',
     'ngMaterial',
-    'auth', 'home', 'navigation',
+    'security',
     'project', 'language', 'technology'
 ])
     .config(['$locationProvider', '$routeProvider', '$mdThemingProvider', '$httpProvider',
         function ($locationProvider, $routeProvider, $mdThemingProvider, $httpProvider) {
             $mdThemingProvider.theme('default').dark();
-            $routeProvider.when('/', {
-                templateUrl: 'app/home/home.html',
-                controller: 'homeController',
-                controllerAs: 'vm'
-            }).otherwise('/');
             $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
         }])
     .constant('_',
