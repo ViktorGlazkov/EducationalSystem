c4a58be55c7454e9dfd7918819456b18544256d7
Viktor Glazkov <viktorg.dev@gmail.com>
2017-03-07 01:21:42 +0200

    Remove extra code

--- client/app/home/home.module.js
+++
@@ -1,9 +0,0 @@
-angular.module('home', ['ngRoute'])
-    .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider
-            .when('/home', {
-                templateUrl: 'app/home/home.html',
-                controller: 'homeController',
-                controllerAs: 'vm'
-            })
-    }]);
--- client/app/navigation/navigation.module.js
+++
@@ -1,9 +0,0 @@
-angular.module('navigation', ['ngRoute', 'auth'])
-    .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider
-            .when('/navigation', {
-                templateUrl: 'app/navigation/navigation.html',
-                controller: 'navigationController',
-                controllerAs: 'vm'
-            })
-    }]);
