e1b5b71cf2862e8004716e4e51cad9267ad14cbf
Viktor Glazkov <viktorg.dev@gmail.com>
2017-03-10 20:11:16 +0200

    Add matchers

--- api/src/main/java/config/ApplicationSecurity.java
+++ api/src/main/java/config/ApplicationSecurity.java
@@ -9,8 +9,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
@@ -18,6 +18,7 @@ import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
+
 
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
@@ -28,6 +29,9 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
     private AuthenticationFailureHandler authenticationFailureHandler;
     @Autowired
     private AuthenticationSuccessHandler authenticationSuccessHandler;
+//    @Autowired
+//    private UserDetailsServiceImpl userDetailsService;
+
     @Autowired
     private CORSFilter corsFilter;
     @Autowired
@@ -35,16 +39,26 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+       // builder.userDetailsService(userDetailsService);
+    //    builder.authenticationProvider(authProvider);
         builder.inMemoryAuthentication().withUser("user").password("user").roles("USER").and().withUser("admin")
                 .password("admin").roles("ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS, "/*/**").permitAll()
-                .antMatchers("/login", "/api/**").permitAll()
-                .antMatchers("/logout", "/rest/**").authenticated();
+        http.httpBasic().and().authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/api/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/**").permitAll()
+
+                .antMatchers(HttpMethod.POST, "/api/user/**").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/api/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/*/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/*/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/*/**").hasRole("ADMIN")
+
+                .antMatchers("/api/login", "/api/open/**").permitAll()
+                .antMatchers("/logout", "/api/**").hasRole("USER");
 
         http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint);
         http.formLogin().successHandler(authenticationSuccessHandler);
@@ -52,18 +66,16 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
         http.logout().logoutUrl("/logout").logoutSuccessHandler(logoutSuccessHandler);
 
-        http.addFilterBefore(corsFilter, ChannelProcessingFilter.class);
-
         http.csrf().requireCsrfProtectionMatcher(
                 new AndRequestMatcher(
-                        new NegatedRequestMatcher(new AntPathRequestMatcher("/login*/**", HttpMethod.OPTIONS.toString())),
+                        new NegatedRequestMatcher(new AntPathRequestMatcher("/api/login*/**", HttpMethod.OPTIONS.toString())),
                         new NegatedRequestMatcher(new AntPathRequestMatcher("/logout*/**", HttpMethod.OPTIONS.toString())),
 
                         new NegatedRequestMatcher(new AntPathRequestMatcher("/api*/**", HttpMethod.GET.toString())),
                         new NegatedRequestMatcher(new AntPathRequestMatcher("/api*/**", HttpMethod.HEAD.toString())),
                         new NegatedRequestMatcher(new AntPathRequestMatcher("/api*/**", HttpMethod.OPTIONS.toString())),
-                        new NegatedRequestMatcher(new AntPathRequestMatcher("/api*/**", HttpMethod.TRACE.toString()))
-                      //  new NegatedRequestMatcher(new AntPathRequestMatcher("/api/**/**"))
+                        new NegatedRequestMatcher(new AntPathRequestMatcher("/api*/**", HttpMethod.TRACE.toString())),
+                        new NegatedRequestMatcher(new AntPathRequestMatcher("/api/open*/**"))
                 )
         );
         http.addFilterAfter(new CsrfTokenResponseCookieBindingFilter(), CsrfFilter.class);
