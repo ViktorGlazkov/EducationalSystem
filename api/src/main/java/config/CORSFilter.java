34d4771b7d354fcbe107bef904480cb2150c8582
Viktor Glazkov <viktorg.dev@gmail.com>
2017-03-11 23:37:57 +0200

    Add oauth2 authentication via google

---
+++ api/src/main/java/config/CORSFilter.java
@@ -0,0 +1,44 @@
+package config;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class CORSFilter implements Filter {
+
+    private final List<String> allowedOrigins = Arrays.asList("http://localhost:3000", "http://localhost:8181", "http://127.0.0.1:8181");
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        if (req instanceof HttpServletRequest && res instanceof HttpServletResponse) {
+            HttpServletRequest request = (HttpServletRequest) req;
+            HttpServletResponse response = (HttpServletResponse) res;
+
+            String origin = request.getHeader("Origin");
+            response.setHeader("Access-Control-Allow-Origin", allowedOrigins.contains(origin) ? origin : "");
+            response.setHeader("Vary", "Origin");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+            response.setHeader("Access-Control-Allow-Headers",
+                    "Origin, X-Requested-With, Content-Type, Accept, X-CSRF-TOKEN");
+        }
+
+        chain.doFilter(req, res);
+    }
+
+    public void init(FilterConfig filterConfig) {
+    }
+}
+
---
+++ api/src/main/java/config/OAuthSecurityConfig.java
@@ -0,0 +1,84 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfFilter;
+
+@Configurable
+@EnableWebSecurity
+public class OAuthSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private OAuth2ClientContext oauth2ClientContext;
+    private AuthorizationCodeResourceDetails authorizationCodeResourceDetails;
+    private ResourceServerProperties resourceServerProperties;
+
+    @Autowired
+    public void setOauth2ClientContext(OAuth2ClientContext oauth2ClientContext) {
+        this.oauth2ClientContext = oauth2ClientContext;
+    }
+
+    @Autowired
+    public void setAuthorizationCodeResourceDetails(AuthorizationCodeResourceDetails authorizationCodeResourceDetails) {
+        this.authorizationCodeResourceDetails = authorizationCodeResourceDetails;
+    }
+
+    @Autowired
+    public void setResourceServerProperties(ResourceServerProperties resourceServerProperties) {
+        this.resourceServerProperties = resourceServerProperties;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/**.html", "/**.js").permitAll()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .logout().logoutSuccessUrl("/").permitAll()
+                .and()
+                .addFilterAt(filter(), BasicAuthenticationFilter.class)
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        http.addFilterAfter(new CORSFilter(), ChannelProcessingFilter.class);
+
+    }
+
+    private OAuth2ClientAuthenticationProcessingFilter filter() {
+        OAuth2ClientAuthenticationProcessingFilter oAuth2Filter = new OAuth2ClientAuthenticationProcessingFilter(
+                "/api/google/login");
+
+        OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(authorizationCodeResourceDetails,
+                oauth2ClientContext);
+        oAuth2Filter.setRestTemplate(oAuth2RestTemplate);
+
+        oAuth2Filter.setTokenServices(new UserInfoTokenServices(resourceServerProperties.getUserInfoUri(),
+                resourceServerProperties.getClientId()));
+
+        return oAuth2Filter;
+    }
+}
