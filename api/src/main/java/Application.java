b3a9b82163369dc3ab29643f2a9ddfd22d592915
Viktor Glazkov <viktorg.dev@gmail.com>
2017-03-11 23:36:50 +0200

    EnableOAuth2Sso

--- api/src/main/java/Application.java
+++ api/src/main/java/Application.java
@@ -1,28 +1,18 @@
 
-import config.cors.CORSFilter;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@EnableJpaRepositories("/core")
-@EntityScan("/core")
+@SpringBootApplication
 @ComponentScan(value = {"/core", "/config"})
-@EnableAutoConfiguration
+@EnableJpaRepositories(value = "/core")
+@EntityScan(value = "/core")
+@EnableOAuth2Sso
 public class Application {
-
-    @Bean
-    public CORSFilter corsFilter() {
-        return new CORSFilter();
-    }
-
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Application.class);
-        application.run(args);
+        SpringApplication.run(Application.class, args);
     }
 }
