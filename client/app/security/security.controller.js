afc65de0ca5f33cb7fd7b5b712676299acc301ed
Viktor Glazkov <viktorg.dev@gmail.com>
2017-03-14 18:56:17 +0200

    Add success anf failure callbacks for login

--- client/app/security/security.controller.js
+++ client/app/security/security.controller.js
@@ -1,7 +1,7 @@
 angular.module('security').controller('SecurityController', SecurityController);
-SecurityController.$inject = ['SecurityService'];
+SecurityController.$inject = ['SecurityService', '$location', '$cookies', '$http', '$window'];
 
-function SecurityController(SecurityService) {
+function SecurityController(SecurityService, $location, $cookies, $http, $window) {
     var vm = this;
     vm.user = null;
 
@@ -11,7 +11,7 @@ function SecurityController(SecurityService) {
     getUser();
     
     function login() {
-        SecurityService.login().then(setUser);
+        SecurityService.login().then(obtainAccessToken, errorHandler);
     }
 
     function getUser() {
@@ -29,5 +29,17 @@ function SecurityController(SecurityService) {
 
     function removeUser() {
         vm.user = null;
+    }
+
+    function obtainAccessToken(data) {
+        $http.defaults.headers.common.Authorization = 'Bearer ' + data.data.access_token;
+        var expireDate = new Date(new Date().getTime() + (1000 * data.data.expires_in));
+        $cookies.put("access_token", data.data.access_token, {'expires': expireDate});
+        $window.location.href = "index";
+    }
+
+
+    function errorHandler(error){
+        console.log(error);
     }
 }
