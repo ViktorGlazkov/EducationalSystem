e5740db7c87ac1a22bd38cc213e2d9ddbae11308
Viktor Glazkov <viktorg.dev@gmail.com>
2017-03-14 18:54:18 +0200

    Remove extra import EnableOAuth2Sso

--- api/src/main/java/Application.java
+++ api/src/main/java/Application.java
@@ -2,7 +2,6 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
