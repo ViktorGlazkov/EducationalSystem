c952cfba9d01aa5bf87bd15ef22ff3285b6881ed
Viktor Glazkov <viktorg.dev@gmail.com>
2017-03-15 21:46:26 +0200

    Add accounts.google.com to allowed origins

--- api/src/main/java/config/CORSFilter.java
+++ api/src/main/java/config/CORSFilter.java
@@ -14,7 +14,11 @@ import java.util.List;
 
 public class CORSFilter implements Filter {
 
-    private final List<String> allowedOrigins = Arrays.asList("http://localhost:3000", "http://localhost:8181", "http://127.0.0.1:8181");
+    private final List<String> allowedOrigins = Arrays.asList(
+            "http://localhost:3000",
+            "http://localhost:8181",
+            "http://127.0.0.1:8181",
+            "https://accounts.google.com");
 
     public void destroy() {
     }
@@ -25,13 +29,10 @@ public class CORSFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
 
         String origin = request.getHeader("Origin");
-        response.setHeader("Access-Control-Allow-Origin", allowedOrigins.contains(origin) ? origin : "");
-        response.setHeader("Vary", "Origin");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers",
-                "Authorization, X-Requested-With, Content-Type, Accept, X-CSRF-TOKEN");
+        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept, Origin" +
+                "x-requested-with, origin, authorization,  client-security-toke");
 
         chain.doFilter(req, res);
     }
