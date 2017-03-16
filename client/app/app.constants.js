a35f5b772ed1ae42096ebb304af78936ca78d10a
Viktor Glazkov <viktorg.dev@gmail.com>
2017-03-16 22:48:36 +0200

    Add constants service

---
+++ client/app/app.constants.js
@@ -0,0 +1,7 @@
+angular.module('app').constant('Constants', {
+    API_URL: "http://localhost:8080",
+    GOOGLE: {
+        CLIENT_ID: "CLIENT_ID",
+        USER_INFO_URI: "USER_INFO_URI"
+    }
+});
